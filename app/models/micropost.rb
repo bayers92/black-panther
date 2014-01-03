718e7ab3295230c0f563a73406c91df6ab0c21d5
Brian Ayers <brianc.ayers@gmail.com>
2014-01-03 14:59:15 -0500

    conditional validation

--- app/models/micropost.rb
+++ app/models/micropost.rb
@@ -3,7 +3,7 @@ class Micropost < ActiveRecord::Base
 	default_scope -> { order('updated_at DESC') }
 	validates :user_id, presence: true
 	validates_length_of :content, maximum: 140, too_long: 'Your post must be less than 140 words.',
-                      tokenizer: ->(str) { str.scan(/\w+/) }
+                      tokenizer: ->(str) { str.scan(/\w+/) }, if: :publish?
     validates :title, presence: true
     validates :subtitle, presence: true
 	validates :content, presence: true
