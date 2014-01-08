f2283feb59f434ca4c6099e5244c1ec644c5230d
Brian Ayers <brianc.ayers@gmail.com>
2014-01-07 23:08:50 -0500

    hover blue for up vote

--- app/assets/javascripts/voted.js
+++ app/assets/javascripts/voted.js
@@ -1,10 +1,12 @@
 $(document).ready(function() {
-	if (window.location.href.indexOf('?voted') != -1) {
-		for(var i in localStorage) {
+	if (window.location.href.indexOf('user') != -1) {	
+		for (var i = 0; i < localStorage.length; i++){
 			$("#voteUp_"+localStorage.key(i)).hide();				
 		}
-		comment_number = document.location.href.split('_')[1];
-		$("#voteUp_" + comment_number).hide();
-		localStorage.setItem(comment_number, true);
-    };
+		if (window.location.href.indexOf('?voted') != -1) {
+			comment_number = document.location.href.split('_')[1];
+			$("#voteUp_" + comment_number).hide();
+			localStorage.setItem(comment_number, true);
+	    };
+	};
 });
