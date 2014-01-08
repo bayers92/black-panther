c882649f75dfa3a23efdde0618862b22cb11cff1
Brian Ayers <brianc.ayers@gmail.com>
2014-01-08 14:49:00 -0500

    deleted public/assets

--- app/assets/javascripts/a1-wordCount.js
+++ app/assets/javascripts/a1-wordCount.js
@@ -1,4 +1,4 @@
-$(function() {
+$(document).ready(function() {
     var wordCounts = {};
     $("textarea[id='micropost_content']").keyup(function() {
         var matches = this.value.match(/\b/g);
