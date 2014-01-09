d336d16308e948ab8339c2c310c21fae7512cc6a
Brian Ayers <brianc.ayers@gmail.com>
2014-01-08 22:33:08 -0500

    stlying of comment form on profile view

--- app/models/comment.rb
+++ app/models/comment.rb
@@ -5,4 +5,6 @@ class Comment < ActiveRecord::Base
 	validates :author_id, presence: true
 	validates :content, presence: true
 	validates :vote, presence: true
+	validates_length_of :content, maximum: 15, too_long: ' - Please limit your suggested topic to 15 words.',
+                      tokenizer: ->(str) { str.scan(/\w+/) }
 end
