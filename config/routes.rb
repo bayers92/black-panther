30a0227d81c7f175bd849c3786c590fb04781793
Brian Ayers <brianc.ayers@gmail.com>
2014-01-09 00:13:40 -0500

    nothing

--- config/routes.rb
+++ config/routes.rb
@@ -10,8 +10,7 @@ Oneforty::Application.routes.draw do
   match '/help',    to: 'static_pages#help',    via: 'get'
   match '/about',   to: 'static_pages#about',   via: 'get'
   match '/contact', to: 'static_pages#contact', via: 'get'
-  match '/increase', to: 'comments#increase', via: [:get, :post]
-
+  match '/increase', to: 'comments#increase',   via: [:get, :post]
   
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
